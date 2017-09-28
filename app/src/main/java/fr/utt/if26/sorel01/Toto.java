579192a90da608ce81bb38d014591ea54ca94ce2
marc lemercier <marc.lemercier@gmail.com>
2017-09-28 13:49:52 +0200

    toto ajouter

--- app/src/main/java/fr/utt/if26/sorel01/Toto.java
+++ app/src/main/java/fr/utt/if26/sorel01/Toto.java
@@ -6,5 +6,22 @@ package fr.utt.if26.sorel01;
 
 public class Toto {
     int toto;
+
+    public Toto(int toto) {
+        this.toto = toto;
+    }
+
+
+    public int getToto() {
+        return toto;
+    }
+
+    public void setToto(int toto) {
+        this.toto = toto;
+    }
+
+
+
+
 }
 
